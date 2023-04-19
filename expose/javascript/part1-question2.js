b5036f2b4d75b88371988ff6d88f036d5c1f0d2f
ChayPark <99235173+ChayPark@users.noreply.github.com>
2023-04-19 11:03:56 -0700

    Update part1-question2.js

--- expose/javascript/part1-question2.js
+++ expose/javascript/part1-question2.js
@@ -1,5 +1,3 @@
 Line 13 prints the message "final result: " followed by the value of the result variable.
-
 However, if add is false or any falsy value, the function will immediately return without executing line 7-12, which means that result will not be defined. In that case, line 13 will cause a ReferenceError because the result variable is not defined.
-
 If add is true, line 13 will print "final result: " followed by the value of result, which is the sum of num1 and num2 in this case.
