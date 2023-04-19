fda1d5cb98b371fbaf4515f38dd70d40bb2285c2
ChayPark <99235173+ChayPark@users.noreply.github.com>
2023-04-19 10:40:39 -0700

    Update part1-question5.js

--- expose/javascript/part1-question5.js
+++ expose/javascript/part1-question5.js
@@ -1,3 +1,2 @@
-Line 12 (console.log(i);) will cause a reference error because i is declared inside the for loop block and is not accessible outside of it. 
-If the function is called like the others with discountPrices([100, 200, 300], 0.5), the for loop will run three times, but i will only be accessible within the for loop's block scope. 
-Therefore, the code will throw a reference error when it tries to access i outside of the for loop.
+At line 12, a ReferenceError will be thrown because i is declared using let inside the for loop block, which means its scope is limited to the loop block only. 
+Therefore, i will not be accessible outside the loop block, and attempting to log its value using console.log(i) will result in a ReferenceError.
