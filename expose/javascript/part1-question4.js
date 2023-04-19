23814f21b2ca34f5b6539d24975d7fe288c4351e
ChayPark <99235173+ChayPark@users.noreply.github.com>
2023-04-19 11:14:26 -0700

    Update part1-question4.js

--- expose/javascript/part1-question4.js
+++ expose/javascript/part1-question4.js
@@ -1,4 +1,7 @@
-This function will return an array containing the discounted prices of the input array. 
-The input array is [100, 200, 300] and the discount is 0.5, so each price in the array will be multiplied by (1 - 0.5) which gives a 50% discount. 
-The discounted prices are then rounded to two decimal places using Math.round and pushed to a new array called discounted. 
-The final discounted prices array will be [50, 100, 150]. The function doesn't cause any errors.
+On line 13, the function will log final result: <result> to the console, where <result> is the value of the result variable.
+In this case, since add is true, the code inside the if statement will be executed and result will be assigned the value of num1 + num2, which is 20. 
+Then, the function will log 'values added: 20' to the console on line 7.
+After that, the code will continue to execute and reach line 13, where the function will log 'final result: 20' to the console, since the value of result is still 20.
+So the output will be: 
+values added: 20
+final result: 20
