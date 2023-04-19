8cc1bf5562d826596b763cdee5e9a59f5978ffde
ChayPark <99235173+ChayPark@users.noreply.github.com>
2023-04-18 22:46:12 -0700

    Create part1-question1.js

---
+++ expose/javascript/part1-question1.js
@@ -0,0 +1,2 @@
+Line 9 (console.log('final result: ', result);) will print the value of result to the console.
+The given code may result in an error because the result variable has not been defined, which can cause a ReferenceError to be thrown.
