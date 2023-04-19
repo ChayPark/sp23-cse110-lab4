8b14aac880cf8c1787269f10f431686c0c8613ff
ChayPark <99235173+ChayPark@users.noreply.github.com>
2023-04-19 10:52:46 -0700

    Create part1-question7.js

---
+++ expose/javascript/part1-question7.js
@@ -0,0 +1,4 @@
+At line 14, the function will log the value of the finalPrice variable to the console. 
+The value of finalPrice will be the final discounted price of the last item in the prices array.
+Since the code does not contain any errors, the function will execute without any issues and log the expected output to the console. 
+The output will be the final discounted price of the last item in the prices array, which in this case would be 150.
