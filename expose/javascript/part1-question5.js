f9ccdf0c3c97bce28d85f1d32817f891a0a9cfea
ChayPark <99235173+ChayPark@users.noreply.github.com>
2023-04-19 11:24:52 -0700

    Update part1-question5.js

--- expose/javascript/part1-question5.js
+++ expose/javascript/part1-question5.js
@@ -1,2 +1,4 @@
-At line 12, a ReferenceError will be thrown because i is declared using let inside the for loop block, which means its scope is limited to the loop block only. 
-Therefore, i will not be accessible outside the loop block, and attempting to log its value using console.log(i) will result in a ReferenceError.
+The output of line 9 would be "values added: 20". This is because the function adds the two numbers and logs the result to the console on line 6.
+However, line 5 to declare result as a constant variable and then try to reassign it on line 7, we will get a TypeError: "Assignment to constant variable" error at runtime. 
+This is because constant variables cannot be reassigned after they are initialized.
+So, in summary, the output of line 9 would be "values added: 20" and there would be an error if we try to reassign the constant variable result on line 7.
