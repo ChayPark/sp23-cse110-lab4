77ddaafb99b466fbb4afa6954dd2b681a8f7d3cb
ChayPark <99235173+ChayPark@users.noreply.github.com>
2023-04-20 21:58:40 -0700

    Update part1-question7.js

--- expose/javascript/part1-question7.js
+++ expose/javascript/part1-question7.js
@@ -1,4 +1,5 @@
-At line 14, the function will log the value of the finalPrice variable to the console. 
-The value of finalPrice will be the final discounted price of the last item in the prices array.
-Since the code does not contain any errors, the function will execute without any issues and log the expected output to the console. 
-The output will be the final discounted price of the last item in the prices array, which in this case would be 150.
+At line 12, the console will print the value of the variable i, which is the index of the last element in the prices array that was processed in the for loop. Since the for loop processes all elements in the prices array, the value of i will be equal to the length of the array, which is 3 in this case.
+Therefore, when the code is executed, the console will print the value 3.
+This code will not cause an error, and the output will be:
+3
+[50, 100, 150]
