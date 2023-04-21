bb272f83699a29cb7fb2b03810402ade7382fed2
ChayPark <99235173+ChayPark@users.noreply.github.com>
2023-04-20 22:13:04 -0700

    Create part1-question10.js

---
+++ expose/javascript/part1-question10.js
@@ -0,0 +1,5 @@
+This function will return an array [50, 100, 150], which contains the discounted prices of the original prices passed as the first argument to the function. 
+The function loops through each price, calculates the discounted price based on the discount rate passed as the second argument, rounds the result to two decimal places using Math.round(), and then pushes the final discounted price into a new array. 
+The final array of discounted prices is returned by the function.
+There are no errors in the given code, so it will not cause any runtime errors. 
+The function discountPrices() will execute as expected and return an array of discounted prices.
