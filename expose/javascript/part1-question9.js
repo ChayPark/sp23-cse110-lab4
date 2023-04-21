020fb3573f786d61a8031c2449bca5b8d15cc3c5
ChayPark <99235173+ChayPark@users.noreply.github.com>
2023-04-20 22:07:54 -0700

    Create part1-question9.js

---
+++ expose/javascript/part1-question9.js
@@ -0,0 +1,4 @@
+At line 14, the finalPrice variable will be logged to the console for each iteration of the for loop. 
+Since the variable is declared inside the loop, it will be re-assigned a new value for each iteration, and only the final value will be logged to the console when the loop ends. 
+Therefore, the console will output the final discounted price of the last item in the prices array (which is 150 in this case) after rounding to two decimal places. 
+There won't be any error.
