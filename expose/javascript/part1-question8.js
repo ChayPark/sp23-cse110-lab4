de733057daa4d3cb9ccab49080803f002bf86996
ChayPark <99235173+ChayPark@users.noreply.github.com>
2023-04-20 22:04:11 -0700

    Create part1-question8.js

---
+++ expose/javascript/part1-question8.js
@@ -0,0 +1,2 @@
+On line 13, an error will occur since discountedPrice is declared inside the for loop and is not accessible outside of it. 
+Therefore, trying to log discountedPrice outside of the loop will result in a ReferenceError: discountedPrice is not defined.
