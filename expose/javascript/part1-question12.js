966e278c38bd7423e7ffd2f0a6c66c4e3644d890
ChayPark <99235173+ChayPark@users.noreply.github.com>
2023-04-23 00:44:04 -0700

    Create part1-question12.js

---
+++ expose/javascript/part1-question12.js
@@ -0,0 +1,3 @@
+At line 13, the function will throw a ReferenceError because the variable discountedPrice is declared with the let keyword inside the for loop and is therefore only accessible within the loop's block scope.
+Since the console.log(discountedPrice) statement is outside of the loop block, discountedPrice is not defined and will result in an error. 
+However, the console.log(discountedPrice) statement will not affect the returned value of the function, which will still be the array [50, 100, 150] representing the discounted prices after applying the discount rate of 0.5 to the original prices of [100, 200, 300].
