407799652222e6c0f3f97d264963e7219edaf370
ChayPark <99235173+ChayPark@users.noreply.github.com>
2023-04-23 00:18:50 -0700

    Update part1-question10.js

--- expose/javascript/part1-question10.js
+++ expose/javascript/part1-question10.js
@@ -1,5 +1,5 @@
-This function will return an array [50, 100, 150], which contains the discounted prices of the original prices passed as the first argument to the function. 
-The function loops through each price, calculates the discounted price based on the discount rate passed as the second argument, rounds the result to two decimal places using Math.round(), and then pushes the final discounted price into a new array. 
-The final array of discounted prices is returned by the function.
-There are no errors in the given code, so it will not cause any runtime errors. 
-The function discountPrices() will execute as expected and return an array of discounted prices.
+The function will return an array of discounted prices for the input array of prices. Specifically, it will return the array [50, 100, 150], which represents a 50% discount applied to each of the original prices of [100, 200, 300], respectively.
+The function works by iterating through the input array of prices and applying the discount to each price, then rounding the resulting discounted price to two decimal places using the Math.round() method. 
+Finally, the discounted price is added to the discounted array using the push() method.
+There is a minor error in the code, however. In the line var discountedPrice = price[i]*(1-discount);, the variable price should be prices, which is the name of the input array parameter. 
+This typo could cause a ReferenceError when running the function, preventing it from executing correctly.
