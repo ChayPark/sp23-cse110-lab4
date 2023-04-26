b6a11de604c252fc2247f7c5f687123473aba0ac
ChayPark <99235173+ChayPark@users.noreply.github.com>
2023-04-26 00:08:37 -0700

    Delete part1-question3.js

--- expose/javascript/part1-question3.js
+++
@@ -1,4 +0,0 @@
-The output of line 9 will be "values added: 20". 
-This is because when the function is called with sumValues(10, 10, true), the condition in the if statement evaluates to true and the code inside the if block is executed, which adds num1 and num2 and assigns the result to the variable result. 
-Then the message "values added:" followed by the value of result is printed to the console.
-The code will not return an error. If the code is called with sumValues(10, 10, false) or sumValues(10, 10), the if block will not be executed and nothing will be printed to the console.
