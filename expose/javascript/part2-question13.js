cffaf1fa2eef616048e3a36f119af90af963d9a3
ChayPark <99235173+ChayPark@users.noreply.github.com>
2023-04-26 00:54:28 -0700

    Delete part2-question13.js

--- expose/javascript/part2-question13.js
+++
@@ -1,9 +0,0 @@
-
-A. '32' - The plus operator concatenates the string '3' with the number 2, resulting in the string '32'.
-B. 1 - The minus operator subtracts the number 2 from the string '3', resulting in the number 1.
-C. 3 - The null value is converted to 0 in numeric contexts, so 3 + 0 results in 3.
-D. '3null' - The plus operator concatenates the string '3' with the string representation of null, which is 'null'.
-E. 4 - The boolean value true is converted to 1 in numeric contexts, so 1 + 3 results in 4.
-F. NaN - The boolean value false is converted to 0 in numeric contexts, and null is converted to 0 as well. Adding 0 to 0 results in 0, which is Not a Number (NaN) when converted to a number.
-G. '3undefined' - The plus operator concatenates the string '3' with the string representation of undefined, which is 'undefined'.
-H. NaN - The minus operator cannot subtract undefined from the string '3', so it results in NaN.
