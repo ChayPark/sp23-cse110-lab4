b457b3bde98c47771035431e8fd13a526155f7d8
ChayPark <99235173+ChayPark@users.noreply.github.com>
2023-04-26 00:43:41 -0700

    Delete part2-question8.js

--- expose/javascript/part2-question8.js
+++
@@ -1,4 +0,0 @@
-The discountPrices function, when called with the arguments [100, 200, 300] and 0.5, will return the array [50, 100, 150].
-This is because the function takes two parameters, an array of prices and a discount value as a decimal. The for loop iterates through each element of the prices array, calculates the discounted price by multiplying the original price by (1 - discount), rounds the result to two decimal places using Math.round(), and adds the final discounted price to the discounted array using the push() method. Finally, the function returns the discounted array.
-In this case, for the prices [100, 200, 300] and a discount rate of 0.5, the discounted prices will be [50, 100, 150], which will be returned by the function.
-There is no error in the function.
