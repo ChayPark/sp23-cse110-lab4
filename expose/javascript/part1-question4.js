d926fc8afb21045b5f021df633d9f9550e0463f8
ChayPark <99235173+ChayPark@users.noreply.github.com>
2023-04-26 00:09:48 -0700

    Delete part1-question4.js

--- expose/javascript/part1-question4.js
+++
@@ -1,7 +0,0 @@
-On line 13, the function will log final result: <result> to the console, where <result> is the value of the result variable.
-In this case, since add is true, the code inside the if statement will be executed and result will be assigned the value of num1 + num2, which is 20. 
-Then, the function will log 'values added: 20' to the console on line 7.
-After that, the code will continue to execute and reach line 13, where the function will log 'final result: 20' to the console, since the value of result is still 20.
-So the output will be: 
-values added: 20
-final result: 20
