668b2676c63f037cec5b473b394373dd5a29f202
ChayPark <99235173+ChayPark@users.noreply.github.com>
2023-04-26 01:09:25 -0700

    Delete part2-question19.js

--- expose/javascript/part2-question19.js
+++
@@ -1,3 +0,0 @@
-When printNums() function is called, it first prints 1 to the console, then schedules a setTimeout function to print 2 after 1000ms (1 second), and schedules another setTimeout function to print 3 after 0ms (immediately). 
-Finally, it prints 4 to the console.
-Therefore, the output will be 1 4 3 2.
