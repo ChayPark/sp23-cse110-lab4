437f988b03103a2ee32b43d203d5a1dc1647dd57
ChayPark <99235173+ChayPark@users.noreply.github.com>
2023-04-26 00:05:53 -0700

    Delete part1-question1.js

--- expose/javascript/part1-question1.js
+++
@@ -1,3 +0,0 @@
-Line 9 (console.log('final result: ', result);) will print the value of result to the console.
-However, in the given code, the console.log statement is outside of the if block, so it will always be executed, even if add is falsy and the result variable has not been defined. 
-Therefore, this situation can cause a ReferenceError will be thrown, since result is not defined.
