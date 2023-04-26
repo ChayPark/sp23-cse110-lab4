ca73b41423bee02fdc5b334889bd2308b4408c2e
ChayPark <99235173+ChayPark@users.noreply.github.com>
2023-04-26 00:16:04 -0700

    Delete part1-question5.js

--- expose/javascript/part1-question5.js
+++
@@ -1,4 +0,0 @@
-The output of line 9 would be "values added: 20". This is because the function adds the two numbers and logs the result to the console on line 6.
-However, line 5 to declare result as a constant variable and then try to reassign it on line 7, we will get a TypeError: "Assignment to constant variable" error at runtime. 
-This is because constant variables cannot be reassigned after they are initialized.
-So, in summary, the output of line 9 would be "values added: 20" and there would be an error if we try to reassign the constant variable result on line 7.
