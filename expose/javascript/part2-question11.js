c14fe20f42baf38a46674ffc7b3270953a41d0c5
ChayPark <99235173+ChayPark@users.noreply.github.com>
2023-04-26 00:50:13 -0700

    Delete part2-question11.js

--- expose/javascript/part2-question11.js
+++
@@ -1,4 +0,0 @@
-The function will return an array containing the discounted prices of the products provided in the prices array. 
-The function calculates the discounted price of each product using the discount percentage provided as the discount parameter and adds it to a new discounted array. 
-It then returns this array.
-There are no errors in the code.
