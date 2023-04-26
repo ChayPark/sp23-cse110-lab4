0ce2ca03765aafe5aeae3bc55215838e7a9349e4
ChayPark <99235173+ChayPark@users.noreply.github.com>
2023-04-26 00:49:42 -0700

    Delete part2-question10.js

--- expose/javascript/part2-question10.js
+++
@@ -1,4 +0,0 @@
-.0.
-At line 12, the console will log the value of the length variable which is equal to the length of the prices array passed as the first argument to the discountPrices function. 
-In this case, the length of the array is 3. 
-The code will not cause any errors as the length variable is defined correctly and accessible within the function scope.
