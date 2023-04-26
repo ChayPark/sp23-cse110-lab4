85b3d7ca8102de09fbfac6592bc7a5ba0b9ad7ac
ChayPark <99235173+ChayPark@users.noreply.github.com>
2023-04-26 00:23:00 -0700

    Delete part2-question1.js

--- expose/javascript/part2-question1.js
+++
@@ -1,5 +0,0 @@
-At line 12, the console will print the value of the variable i, which is the index of the last element in the prices array that was processed in the for loop. Since the for loop processes all elements in the prices array, the value of i will be equal to the length of the array, which is 3 in this case.
-Therefore, when the code is executed, the console will print the value 3.
-This code will not cause an error, and the output will be:
-3
-[50, 100, 150]
